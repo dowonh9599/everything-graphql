bfee905f6a34e8dfc06c3ad7e94c1bc4da6dfbdf
dowonh9599 <hdwkr95.developer@gmail.com>
2022-07-17 21:58:51 +0800

    graphql resolver setup

---
+++ schema/resolvers.ts
@@ -0,0 +1,9 @@
+import USER_DATA from "./USER_DATA.json";
+import MOVIE_DATA from "./MOVIE_DATA.json";
+import { User, Movie, CreateUserInput, UpdateUsernameInput } from "./types";
+const _ = require("lodash");
+
+const resolvers = {
+};
+
+export { resolvers };
