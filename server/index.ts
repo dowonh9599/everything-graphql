3424f4cc09b1673e157f69c75116c72d5e59c88f
dowonh9599 <hdwkr95.developer@gmail.com>
2022-07-17 22:09:56 +0800

    refactor: file rearranged

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import { ApolloServer } from "apollo-server";
-import { typeDefs } from "../schema/typeDefs";
-import { resolvers } from "../schema/resolvers";
+import { typeDefs } from "./schema/typeDefs";
+import { resolvers } from "./schema/resolvers";
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => {
